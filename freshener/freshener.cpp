ac1da476c0553597d8e8eb4565c9d43e76546724
Pavel Savinov <swapii@gmail.com>
2016-12-30 06:49:13 +0300

    Power down when waiting events from sensor

--- freshener/freshener.cpp
+++ freshener/freshener.cpp
@@ -1,9 +1,12 @@
 #include <Arduino.h>
+#include <LowPower.h>
 
 #define LIGHT_SERSOR_PIN A0
 #define LED_PIN 13
 
 #define LIGHT_LEVEL_THRESHOLD 500
+
+unsigned long elapsedTime;
 
 void setup(void) {
     pinMode(LIGHT_SERSOR_PIN, INPUT_PULLUP);
@@ -14,10 +17,12 @@ void loop(void) {
 
     if (analogRead(LIGHT_SERSOR_PIN) > LIGHT_LEVEL_THRESHOLD) {
         digitalWrite(LED_PIN, HIGH);
+        elapsedTime += 500;
+        LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
     } else {
         digitalWrite(LED_PIN, LOW);
+        elapsedTime += 4000;
+        LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
     }
-
-    delay(100);
 
 }
