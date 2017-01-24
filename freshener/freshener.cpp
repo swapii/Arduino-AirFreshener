4746adef9de0112d089b5b14d040e951b9789600
Pavel Savinov <swapii@gmail.com>
2017-01-24 22:34:51 +0300

    Handle motor with simple logic
    Compare two light states (old and new).
    
    Turn on motor by pin 10 when light is switched off.
    
    Wait (sleep) some time after air fresh.

--- freshener/freshener.cpp
+++ freshener/freshener.cpp
@@ -2,27 +2,53 @@
 #include <LowPower.h>
 
 #define LIGHT_SERSOR_PIN A0
-#define LED_PIN 13
+#define MOTOR_PIN 10
 
 #define LIGHT_LEVEL_THRESHOLD 500
 
-unsigned long elapsedTime;
+bool isLightWasOn = false;
+
+void sleepOneMinute();
 
 void setup(void) {
     pinMode(LIGHT_SERSOR_PIN, INPUT_PULLUP);
-    pinMode(LED_PIN, OUTPUT);
+    pinMode(MOTOR_PIN, OUTPUT);
 }
 
 void loop(void) {
 
-    if (analogRead(LIGHT_SERSOR_PIN) > LIGHT_LEVEL_THRESHOLD) {
-        digitalWrite(LED_PIN, HIGH);
-        elapsedTime += 500;
-        LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
-    } else {
-        digitalWrite(LED_PIN, LOW);
-        elapsedTime += 4000;
-        LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
+    bool isLightOnNow = analogRead(LIGHT_SERSOR_PIN) > LIGHT_LEVEL_THRESHOLD;
+
+    bool isLightSwitchedOff = !isLightOnNow && isLightWasOn;
+
+    isLightWasOn = isLightOnNow;
+
+    if (isLightSwitchedOff) {
+
+        // Wait 1 second after light off
+        LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+
+        digitalWrite(MOTOR_PIN, HIGH);
+        LowPower.powerDown(SLEEP_1S, ADC_OFF, BOD_OFF);
+        digitalWrite(MOTOR_PIN, LOW);
+
+        // No need to fresh air right after previous fresh
+        sleepOneMinute();
+        sleepOneMinute();
+
     }
 
+    LowPower.powerDown(SLEEP_500MS, ADC_OFF, BOD_OFF);
+
+}
+
+void sleepOneMinute() {
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+    LowPower.powerDown(SLEEP_4S, ADC_OFF, BOD_OFF);
 }
